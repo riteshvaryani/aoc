a0a8bbc3ea26b9a8a55993f901941f9e10398c3a
Ritesh Varyani <riteshvaryani@gmail.com>
2020-12-24 20:43:15 -0800

    initial aoc commit of 2020

---
+++ 2020/1/1.1/1.1.py
@@ -0,0 +1,15 @@
+import sys
+inputset=set()
+with open('/Users/master/projects/aoc/2020/1/1.1/input.txt') as f:
+    alllines=f.readlines()
+    for input in alllines:
+        intInput=int(input)
+        i=2020-intInput
+        if i in inputset:
+            print(str(i * intInput))
+            exit()
+        inputset.add(intInput)
+
+
+
+    
